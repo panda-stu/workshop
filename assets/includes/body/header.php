a023ae7cef99243102aaf606652d355fa2ea38f8
Soldierplayz6867 <soldierplayz.tw@gmail.com>
2022-12-05 22:22:12 +0800

    Same

--- assets/includes/body/header.php
+++ assets/includes/body/header.php
@@ -2,7 +2,7 @@
 <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-        <h1 class="logo"><a href="https://cloudxact.com">胖達 <span>X</span> 工作室</a></h1>
+        <h1 class="logo"><a href="https://www.pandastudio.ga">胖達 <span>X</span> 工作室</a></h1>
         <!-- <a href="index.html" class="logo"><img src="assets/img/CloudXact.png" alt=""></a>-->
 
         <nav id="navbar" class="navbar">
