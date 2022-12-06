b1d77ddd3a87151b0cd121e9c057d650bc0dd181
Soldierplayz6867 <soldierplayz.tw@gmail.com>
2022-12-06 10:48:02 +0800

    e

---
+++ 404.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="zh-Hant-TW">
+
+<head>
+  <?php include('assets/includes/head/header.php') ?>
+
+  <title>胖達工作室</title>
+
+  <?php include('assets/includes/head/og.php') ?>
+
+  <?php include('assets/includes/head/logo.php') ?>
+
+  <?php include('assets/includes/head/fonts.php') ?>
+
+  <?php include('assets/includes/head/css.php') ?>
+</head>
+
+<body>
+  <?php include('assets/includes/body/topbar.php') ?>
+
+  <?php include('assets/includes/body/header.php') ?>
+
+  <?php include('assets/includes/body/hero.php') ?>
+
+  <?php include('assets/includes/footer/index.php') ?>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- JS檔案 -->
+  <?php include('assets/includes/footer/js.php') ?>
+</body>
+
+</html>
